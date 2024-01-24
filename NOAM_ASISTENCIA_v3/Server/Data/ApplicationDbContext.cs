d192f3ac0ac6fa7b870b34155743c26e713b2661
LAP_LEPECIANA\josem <Jos Lepe>
2024-01-24 14:15:59 -0600

    Proyecto generado en .NET 7 y migrado a .NET 8

---
+++ NOAM_ASISTENCIA_v3/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NOAM_ASISTENCIA_v3.Server.Models;
+
+namespace NOAM_ASISTENCIA_v3.Server.Data;
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(
+        DbContextOptions options,
+        IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+    {
+    }
+}
